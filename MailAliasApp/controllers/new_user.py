acc0dbf7991f379764a3bf1dd33583b31749f5fb
Kabir Gogia <kabirgogia@gmail.com>
2017-07-07 15:09:28 -0700

    Cleaned up post

--- MailAliasApp/controllers/new_user.py
+++ MailAliasApp/controllers/new_user.py
@@ -5,7 +5,7 @@ from MailAliasApp.models import User
 def index(request):
 
 	if request.method == 'POST':
-		user = User.User(first_name="Kakoos", last_name="Swami")
-		user.save()
+		user = User.User(first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+		print user
 
 	return render(request, "NewUserForm.html", {'form': User.NewUserForm()})
